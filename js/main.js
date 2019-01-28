79e1f98b9debd7091dc51aeb5f1a7c38483af312
Joe Carre <joseph.carre@accessionhealth.com>
2019-01-28 10:49:35 +0000

    formatting
    -changed +'s to \'s

--- js/main.js
+++ js/main.js
@@ -22,31 +22,30 @@ function openRequest() {
 
 function buildRequest() {
     var request =
-        '<?xml version="1.0" encoding="utf-8"?>' +
-        '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:bf="http://www.beautyfort.com/api/"> ' +
-            '<soap:Header> ' +
-                '<bf:AuthHeader> AuthHeader ' +
-                '<bf:Username>Joe</bf:Username> ' +
-                '<bf:Nonce> string </bf:Nonce> ' +
-                '<bf:Created> dateTime </bf:Created>' +
-                '<bf:Password>ArbwT1ckBz2ymoEW46ZO5H8alg3uDCUGhXtjseVKxNfM</bf:Password>' +
-            '</soap:Header>' +
-            '<soap:Body>' +
-                '<bf:GetStockFileRequest> GetStockFileRequestType' +
-                    '<bf:TestMode> boolean </bf:TestMode>' +
-                    '<bf:StockFileFormat> StockFileFormat (string) </bf:StockFileFormat>' +
-                    '<bf:FieldDelimiter> StringLength1 (string) </bf:FieldDelimiter>' +
-                    '<bf:StockFileFields> ArrayOfStockFileField' +
-                        '<bf:StockFileField> StockFileField (string) </bf:StockFileField>' +
-                    '</bf:StockFileFields>' +
-                    '<bf:SortBy> StockFileSort (string) </bf:SortBy>' +
-                '</bf:GetStockFileRequest>' +
-            '</soap:Body>' +
-        '</soap:Envelope>';
+        '<?xml version="1.0" encoding="utf-8"?>\
+        <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:bf="http://www.beautyfort.com/api/">\
+            <soap:Header>\
+                <bf:AuthHeader> AuthHeader\
+                <bf:Username>Joe</bf:Username>\
+                <bf:Nonce> string </bf:Nonce>\
+                <bf:Created> dateTime </bf:Created>\
+                <bf:Password>ArbwT1ckBz2ymoEW46ZO5H8alg3uDCUGhXtjseVKxNfM</bf:Password>\
+            </soap:Header>\
+            <soap:Body>\
+                <bf:GetStockFileRequest> GetStockFileRequestType\
+                    <bf:TestMode> boolean </bf:TestMode>\
+                    <bf:StockFileFormat> StockFileFormat (string) </bf:StockFileFormat>\
+                    <bf:FieldDelimiter> StringLength1 (string) </bf:FieldDelimiter>\
+                    <bf:StockFileFields> ArrayOfStockFileField\
+                    <bf:StockFileField> StockFileField (string) </bf:StockFileField>\
+                    </bf:StockFileFields>\
+                    <bf:SortBy> StockFileSort (string) </bf:SortBy>\
+                </bf:GetStockFileRequest>\
+            </soap:Body>\
+        </soap:Envelope>';
         return request;
 };
 
 $('.btn').click(function() {
-    console.log('this is working');
     sendRequest(buildRequest());
 });
